a67344698433b403fc405a6f36f4cb51821600d8
Joaquin Costa <joaquin.costa@simulart.es>
2025-07-15 11:17:08 +0200

    fix: edited assetion statement for tests

--- extract/__tests__/i3_download.py
+++ extract/__tests__/i3_download.py
@@ -13,10 +13,8 @@ def test_i3_download_multiple_days():
     try:
 
         i3_volumenes_extractor = I3VolumenesExtractor()
-        overall_success, status_details = i3_volumenes_extractor.extract_data_for_all_markets(fecha_inicio_carga="2024-12-01", fecha_fin_carga="2024-12-02", mercados_lst=["intra"])
-        breakpoint()
-
-        assert overall_success == True, "i3 multiple days download failed"
+        results = i3_volumenes_extractor.extract_data_for_all_markets(fecha_inicio_carga="2024-12-01", fecha_fin_carga="2024-12-02", mercados_lst=["intra"])
+        assert results["success"] == True, "i3 multiple days download failed"
 
     except Exception as e:
         print(f"Error: {e}")
@@ -31,13 +29,13 @@ def test_i3_download_latest_day():
         i3_volumenes_extractor = I3VolumenesExtractor()
         results = i3_volumenes_extractor.extract_data_for_all_markets(fecha_inicio_carga=None, fecha_fin_carga=None, mercados_lst=["diario"])
         assert results["success"] == True, "i3 latest day download failed"
+        
     except Exception as e:
         print(f"Error: {e}")
         raise e
 
 if __name__ == "__main__":
     test_i3_download_latest_day()
-    breakpoint()
     time.sleep(5)
     test_i3_download_multiple_days() 
 
