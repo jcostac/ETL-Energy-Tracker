df2dc6ef3b098dc2870af6df880520e16295a031
Joaquin Costa <jj_costa@outlook.es>
2025-07-16 11:45:39 +0200

    config: curtailment config

--- configs/curtailment_config.py
+++ configs/curtailment_config.py
@@ -20,7 +20,20 @@ class CurtailmentConfig:
         self.rtx_mapping = {"RT1": "03", "RT5": "08"}
         self.rt1_redespacho_filter = ["UPLPVPV", "UPLPVPCBN"]
         self.rt5_redespacho_filter = ["Restricciones Técnicas"]
-        self.i90_sheet_to_market, self.i3_sheet_to_market = self.get_sheet_to_market_mappings()
+        self.i90_sheet_to_market_id, self.i3_sheet_to_market_id = self.get_sheet_to_market_mappings()
+
+        #mapping useful so we can search in the raw file of the correct market folder
+        self.market_id_to_name = {
+            9: "restricciones", #a subir no se va a usar
+            10: "restricciones", 
+            11: "restricciones", #a subir no se va a usar
+            12: "restricciones", 
+            13: "curtailment",
+            22: "indisponibilidades",
+            23: "curtailment", #a subir no se va a usar (ie curtailment demanda)
+            24: "restricciones", #a subir no se va a usar
+            25: "restricciones"#a subir no se va a usar
+        }
 
     @property
     def bbdd_engine(self):
@@ -54,7 +67,7 @@ class CurtailmentConfig:
             columns=['mercado', 'sheet_i90_volumenes'],
             where_clause="sheet_i90_volumenes IN (3, 8)"
         )
-        i90_sheet_to_market = dict(zip(df_i90['sheet_i90_volumenes'].astype(str).str.zfill(2), df_i90['mercado']))
+        i90_sheet_to_market_id = dict(zip(df_i90['sheet_i90_volumenes'].astype(str).str.zfill(2), df_i90['id']))
 
         # Query for i3 sheets 03 and 08
         df_i3 = DatabaseUtils.read_table(
@@ -63,25 +76,23 @@ class CurtailmentConfig:
             columns=['mercado', 'sheet_i3_volumenes'],
             where_clause="sheet_i3_volumenes IN (3, 8)"
         )
-        i3_sheet_to_market = dict(zip(df_i3['sheet_i3_volumenes'].astype(str).str.zfill(2), df_i3['mercado']))
+        i3_sheet_to_market_id= dict(zip(df_i3['sheet_i3_volumenes'].astype(str).str.zfill(2), df_i3['id']))
 
-        return i90_sheet_to_market, i3_sheet_to_market
+        return i90_sheet_to_market_id, i3_sheet_to_market_id
 
-    def get_market_name(self, sheet_num, source='i90'):
+    def get_market_folder(self, market_id: int):
         """
-        Get the market name associated with a given sheet number for i90 or i3.
-        
+        Get the market folder name associated with a given market id.
+
         Args:
-            sheet_num (str): Sheet number like '03' or '08'
-            source (str): 'i90' or 'i3'
-            
+            market_id (int): The market id to get the folder name for.
+
         Returns:
-            str: Market name or None if not found
+            str: The market folder name.
         """
-        if source == 'i90':
-            return self.i90_sheet_to_market.get(sheet_num)
-        elif source == 'i3':
-            return self.i3_sheet_to_market.get(sheet_num)
-        return None
+        return self.market_id_to_name[market_id]
+
+
+        
     
 
